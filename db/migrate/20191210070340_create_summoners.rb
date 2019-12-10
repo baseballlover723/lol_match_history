809e5f13e5ee4bb7e0dabe56ee179ae15346e5ca
Philip Ross <dev@baseballlover723.com>
2019-12-09 23:04:50 -0800

    renamed user to summoner and added region

--- db/migrate/20191210070340_create_summoners.rb
+++ db/migrate/20191210070340_create_summoners.rb
@@ -1,11 +1,12 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
+class CreateSummoners < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
+    create_table :summoners do |t|
       t.string :username
       t.string :puuid
       t.string :summoner_id
       t.string :account_id
       t.bigint :revisionDate
+      t.string :region
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_212448) do
+ActiveRecord::Schema.define(version: 2019_12_10_070340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "summoners", force: :cascade do |t|
+    t.string "username"
+    t.string "puuid"
+    t.string "summoner_id"
+    t.string "account_id"
+    t.bigint "revisionDate"
+    t.string "region"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -21,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_12_09_212448) do
     t.string "summoner_id"
     t.string "account_id"
     t.bigint "revisionDate"
+    t.string "region"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
